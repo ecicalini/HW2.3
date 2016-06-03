e47669f119553b6bbcb814842e4d76566ce3ae09
ecicalini <elena.cicalini@gmail.com>
2016-06-03 20:45:10 +0200

    Solution

--- src/main/java/com/mongodb/m101j/crud/App.java
+++ src/main/java/com/mongodb/m101j/crud/App.java
@@ -20,13 +20,74 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.ReadPreference;
 import com.mongodb.ServerAddress;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Sorts;
 import org.bson.BsonDocument;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.excludeId;
+import static com.mongodb.client.model.Projections.fields;
+import static com.mongodb.client.model.Projections.include;
+import static com.mongodb.client.model.Sorts.ascending;
+import static com.mongodb.client.model.Sorts.descending;
+import static com.mongodb.m101j.util.Helpers.printJson;
 
 public class App {
     public static void main(String[] args) {
+        MongoClient client = new MongoClient();
+        MongoDatabase database = client.getDatabase("students");
+        MongoCollection<Document> collection = database.getCollection("grades");
+
+        // db.grades.find({type:"homework"}).sort({student_id:1},{score:1})
+        Bson filter = eq("type", "homework");
+
+        Bson sort = Sorts.ascending("student_id", "score");
+
+        List<Document> students = collection.find(filter).sort(sort).into(new ArrayList<Document>());
+
+        System.out.println("grades collection initial size: " + collection.count());
+
+        Integer student_id = null;
+        for (Document student : students) {
+            Integer current_student_id = (Integer) student.get("student_id");
+            if(student_id == null){
+                student_id = current_student_id;
+                collection.deleteOne(student);
+            }
+
+            if(!student_id.equals(current_student_id)){
+                student_id = current_student_id;
+                collection.deleteOne(student);
+            }
+        }
+
+        System.out.println("grades collection final size: " + collection.count());
+
+//        db.grades.find().sort({'score':-1}).skip(100).limit(1)
+        List<Document> score = collection.find().sort(descending("score"))
+                                                .skip(100)
+                                                .limit(1)
+                                                .into(new ArrayList<Document>());
+        System.out.println("== db.grades.find().sort({'score':-1}).skip(100).limit(1) ==");
+        printJson(score.get(0));
+
+//        db.grades.find({},{'student_id':1, 'type':1, 'score':1, '_id':0}).sort({'student_id':1, 'score':1, }).limit(5)
+        List<Document> score5 = collection.find().projection(fields(include("student_id", "type", "score"), excludeId()))
+                .sort(ascending("student_id","score"))
+                .limit(5)
+                .into(new ArrayList<Document>());
+
+        System.out.println("== db.grades.find({},{'student_id':1, 'type':1, 'score':1, '_id':0}).sort({'student_id':1, 'score':1, }).limit(5) ==");
+        for (Document doc : score5) {
+            printJson(doc);
+        }
 
     }
 }
